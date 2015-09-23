bb16990fe5231537adee1ab71841c03938f012ff
eyal <eyal@dudamobile.com>
2015-09-24 00:11:23 +0300

    Initial build subtree commit

---
+++ public/0.a38b046e9d47e089f027.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate(0,{
+
+/***/ 304:
+/***/ function(module, exports, __webpack_require__) {
+
+  exports = module.exports = __webpack_require__(16)();
+  // imports
+  
+  
+  // module
+  exports.push([module.id, "/* React Starter Kit | MIT License | http://www.reactstarterkit.com/ */\n\n:root {\n\n  /*\n   * Colors\n   * ======================================================================== */   /* #404040 */ /* #555 */ /* #777 */ /* #eee */\n\n\n  /*\n   * Costum\n   * ======================================================================== */\n\n  /*\n   * Typography\n   * ======================================================================== */\n\n  /*\n   * Layout\n   * ======================================================================== */\n\n  /*\n   * Media queries breakpoints\n   * ======================================================================== */  /* Extra small screen / phone */  /* Small screen / tablet */  /* Medium screen / desktop */ /* Large screen / wide desktop */\n\n  /*\n   * Animations\n   * ======================================================================== */\n\n}\n\n.Projects-inner {\n    display: -webkit-box;\n    display: -webkit-flex;\n    display: -ms-flexbox;\n    display: flex;\n    -webkit-flex-wrap: wrap;\n        -ms-flex-wrap: wrap;\n            flex-wrap: wrap;\n}\n\n.Project-active {\n    position: fixed;\n    height: 100vh;\n    width: 100vw;\n    background-color: #fff;\n    z-index: 10;\n    top: 0;\n    opacity: 0;\n    visibility: hidden;\n    padding-top: 75px;\n    overflow-y: scroll;\n}\n\n.ProjectActive-inner {\n    -webkit-transition: all 0.6s;\n            transition: all 0.6s;\n    -webkit-transform: scale(0.5);\n        -ms-transform: scale(0.5);\n            transform: scale(0.5);\n}\n\n.Projects[data-active=\"true\"] .Project-active {\n  visibility: visible;\n\n}\n\n.Projects[data-active=\"true\"] .ProjectActive-inner {\n  opacity: 1;\n  -webkit-transform: scale(1);\n      -ms-transform: scale(1);\n          transform: scale(1);\n\n}", ""]);
+  
+  // exports
+
+
+/***/ }
+
+})
+//# sourceMappingURL=0.a38b046e9d47e089f027.hot-update.js.map
